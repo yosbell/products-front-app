7ee7c78fbd5069282aca046034c1ee2edd9cf9d9
Yosbel Lázaro <yosbellazaro92@gmail.com>
2025-03-24 23:14:16 -0400

    add: Dialog for show product details

---
+++ components/products/dialog-product.tsx
@@ -0,0 +1,83 @@
+import {
+  Box,
+  Button,
+  CloseButton,
+  Dialog,
+  Image,
+  Portal,
+  Text,
+} from "@chakra-ui/react";
+import { Product } from "./product-list";
+
+interface DialogProductProps {
+  product: Product;
+}
+
+const DialogProduct: React.FC<DialogProductProps> = ({ product }) => {
+  const { name, description, price, image } = product;
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <Button variant="outline" size="sm" padding={"2px 8px"}>
+          Details
+        </Button>
+      </Dialog.Trigger>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header padding={"2"}>
+              <Dialog.Title>Product:</Dialog.Title>
+            </Dialog.Header>
+            <Dialog.Body>
+              <Box p="2">
+                <Image
+                  src={image}
+                  alt={name}
+                  className="product-image"
+                  height={"250px"}
+                  aspectRatio={1.2}
+                />
+                <Text
+                  fontWeight="bold"
+                  fontSize="sm"
+                  className="product-name"
+                  lineClamp="1"
+                >
+                  {name}
+                </Text>
+                <Text
+                  mt="2"
+                  className="product-description"
+                  fontSize={"xs"}
+                  lineClamp="2"
+                >
+                  {description}
+                </Text>
+                <Text
+                  mt="2"
+                  color="teal.500"
+                  fontSize="xs"
+                  className="product-price"
+                >
+                  ${price}
+                </Text>
+              </Box>
+            </Dialog.Body>
+            {/* <Dialog.Footer>
+              <Dialog.ActionTrigger asChild>
+                <Button variant="outline">Cancel</Button>
+              </Dialog.ActionTrigger>
+              <Button>Save</Button>
+            </Dialog.Footer> */}
+            <Dialog.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </Dialog.CloseTrigger>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
+};
+
+export default DialogProduct;
--- components/products/product-card.tsx
+++ components/products/product-card.tsx
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
-import { Box, Image, Text, Checkbox } from "@chakra-ui/react";
+import { Box, Image, Text, Checkbox, Flex } from "@chakra-ui/react";
 import { Product } from "./product-list";
+import DialogProduct from "./dialog-product";
 
 interface ProductCardProps {
   product: Product;
 }
 
 const getPreviewApprovedProducts = () => {
-  return JSON.parse(localStorage.getItem('approved-products') || '{}');
-}
+  return JSON.parse(localStorage.getItem("approved-products") || "{}");
+};
 const getInitialApprovedStatus = (id: number) => {
   return getPreviewApprovedProducts()[id] || false;
-}
+};
 
-const ProductCard: React.FC<ProductCardProps> = ({
-  product: { id, image, name, description, price },
-}) => {
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const { id, image, name, description, price } = product;
   const [approved, setApproved] = useState(getInitialApprovedStatus(id));
 
   const handleCheckboxChange = () => {
     const newStatus = !approved;
     setApproved(newStatus);
-    const previewApprovedProducts = JSON.parse(localStorage.getItem('approved-products') || '{}');
-    localStorage.setItem('approved-products', JSON.stringify({ ...previewApprovedProducts, [id]: newStatus }));
+    const previewApprovedProducts = JSON.parse(
+      localStorage.getItem("approved-products") || "{}"
+    );
+    localStorage.setItem(
+      "approved-products",
+      JSON.stringify({ ...previewApprovedProducts, [id]: newStatus })
+    );
   };
 
   return (
@@ -32,32 +37,51 @@ const ProductCard: React.FC<ProductCardProps> = ({
       overflow="hidden"
       p="2"
       className="product-card"
-      height={'300px'}
-      display={'flex'}
-      flexDirection={'column'}
-      justifyContent={'center'}
+      height={"300px"}
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
     >
-      <Image src={image} alt={name} className="product-image" height={'150px'} aspectRatio={1.2} />
+      <Image
+        src={image}
+        alt={name}
+        className="product-image"
+        height={"150px"}
+        aspectRatio={1.2}
+      />
       <Box p="2">
-        <Text fontWeight="bold" fontSize="sm" className="product-name" lineClamp="1">
+        <Text
+          fontWeight="bold"
+          fontSize="sm"
+          className="product-name"
+          lineClamp="1"
+        >
           {name}
         </Text>
-        <Text mt="2" className="product-description" fontSize={'xs'} lineClamp="2">
+        <Text
+          mt="2"
+          className="product-description"
+          fontSize={"xs"}
+          lineClamp="2"
+        >
           {description}
         </Text>
         <Text mt="2" color="teal.500" fontSize="xs" className="product-price">
           ${price}
         </Text>
-        <Checkbox.Root
-          mt="2"
-          checked={approved}
-          onCheckedChange={handleCheckboxChange}
-          size={'xs'}
-        >
-          <Checkbox.HiddenInput />
-          <Checkbox.Control />
-          <Checkbox.Label fontSize={'xs'}>Aproved</Checkbox.Label>
-        </Checkbox.Root>
+        <Flex justifyContent={"space-between"}>
+          <Checkbox.Root
+            mt="2"
+            checked={approved}
+            onCheckedChange={handleCheckboxChange}
+            size={"xs"}
+          >
+            <Checkbox.HiddenInput />
+            <Checkbox.Control />
+            <Checkbox.Label fontSize={"xs"}>Aproved</Checkbox.Label>
+          </Checkbox.Root>
+          <DialogProduct product={product} />
+        </Flex>
       </Box>
     </Box>
   );
